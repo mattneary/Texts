061a29e46615ba9a1a6b0e2f3c5fdee43a72e3b1
Matt Neary <neary.matt@gmail.com>
2013-12-07 17:23:51 -0500

    make message query building script

---
+++ message_search.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "SELECT 
+  DATETIME(date + 978307200, 'unixepoch', 'localtime') as Date, 
+  h.id as \"Phone Number\", 
+  text as Text 
+FROM message m, handle h 
+WHERE 
+  h.rowid = m.handle_id AND 
+  m.text LIKE '%$1%' 
+ORDER BY m.rowid ASC;"
+
