2f6a4d4cc31d4219c74c45ad8613ba82e30eac7f
Matt Neary <neary.matt@gmail.com>
2013-12-07 17:24:41 -0500

    add functions for the queries based on the person/message scripts

--- functions.fish
+++ functions.fish
@@ -5,3 +5,10 @@ function texts
   echo $argv ";" | sqlite3 $sms_db | sed 's/\|/" "/g' | sed 's/^/"/g' |  sed 's/$/"/g' 
 end
 
+function textsq
+  texts (./message_search.sh $argv)
+end
+function textsp
+  texts (./person_search.sh $argv)
+end
+
