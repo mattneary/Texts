a3e50f5510b39d5ea6caaba6c14b1a758ae82612
Matt Neary <neary.matt@gmail.com>
2013-12-07 17:24:03 -0500

    make person query script

---
+++ person_search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "SELECT
+  DATETIME(date + 978307200, 'unixepoch', 'localtime') as Date, 
+  h.id as \"Phone Number\", 
+  text as Text, 
+  is_from_me as Sent FROM message m, 
+  handle h 
+WHERE 
+  h.rowid = m.handle_id AND 
+  h.id='$1' 
+ORDER BY m.rowid ASC;"
+
